614c043f3080cdbba6b491ac7c8deb81e5b2f8a9
blackcrowx00 <ryanj.kocher94@gmail.com>
2026-03-13 12:43:14 -0500

    Create checkpoints.js

---
+++ checkpoints.js
@@ -0,0 +1,24 @@
+const checkpoints = [
+
+{name:"Anchorage",lat:61.2175,lon:-149.8584},
+{name:"Willow",lat:61.7694,lon:-150.0556},
+{name:"Yentna",lat:61.732,lon:-150.725},
+{name:"Skwentna",lat:61.965,lon:-151.193},
+{name:"Finger Lake",lat:62.705,lon:-152.093},
+{name:"Rainy Pass",lat:62.089,lon:-152.721},
+{name:"Rohn",lat:62.103,lon:-153.377},
+{name:"Nikolai",lat:63.013,lon:-154.383},
+{name:"McGrath",lat:62.956,lon:-155.608},
+{name:"Takotna",lat:62.985,lon:-156.065},
+{name:"Ophir",lat:63.145,lon:-156.529},
+{name:"Cripple",lat:63.905,lon:-157.717},
+{name:"Ruby",lat:64.739,lon:-155.486},
+{name:"Galena",lat:64.736,lon:-156.937},
+{name:"Nulato",lat:64.719,lon:-158.118},
+{name:"Kaltag",lat:64.327,lon:-158.726},
+{name:"Unalakleet",lat:63.873,lon:-160.788},
+{name:"Shaktoolik",lat:64.371,lon:-161.223},
+{name:"Koyuk",lat:64.931,lon:-161.155},
+{name:"Nome",lat:64.501,lon:-165.406}
+
+]
